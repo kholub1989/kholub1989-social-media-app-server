8d4d340f50a6d22a4543f136999b501083578ee3
Krisztian Holub <kholub1989@gmail.com>
2020-10-23 15:26:15 -0400

    init

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  MONGODB:
+    "mongodb+srv://kholub1989:VobMtzS8XolftEHb@cluster0.vnxeb.mongodb.net/DB?retryWrites=true&w=majority",
+  SECRET_KEY: "OMG really secret key",
+};
